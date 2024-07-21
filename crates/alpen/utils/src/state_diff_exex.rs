a32413b8c004ae70389edd3a94aec1770290a3f6
abishek <abishek@alpenlabs.io>
2024-07-21 17:31:19 +0545

    feat: initial input save done

--- crates/alpen/utils/src/state_diff_exex.rs
+++ crates/alpen/utils/src/state_diff_exex.rs
@@ -1,15 +1,17 @@
-use std::sync::Arc;
+use std::{fs::File, io::Write, sync::Arc};
 
+use hashbrown::HashMap;
 use reth_alpen_primitives::state_diff::{StateDiff, StateDiffOp};
 use reth_exex::{ExExContext, ExExEvent, ExExNotification};
 use reth_node_api::FullNodeComponents;
-use reth_primitives::B256;
-use reth_provider::StateProviderFactory;
+use reth_primitives::{Address, TransactionSignedNoHash, B256};
+use reth_provider::{BlockReader, StateProviderFactory};
+use reth_revm::db::BundleState;
+use reth_rpc_types::EIP1186AccountProofResponse;
+use reth_rpc_types_compat::proof::from_primitive_account_proof;
+use serde_json::to_string;
 use tokio::sync::mpsc;
-
-// struct ProverInput {
-//     pub parent_storage: MptNode,
-// }
+use zkvm_primitives::{mpt::proofs_to_tries, SP1RethInput};
 
 /// exex to extract and save state diffs for each block
 pub async fn state_diff_exex<Node: FullNodeComponents>(
@@ -20,8 +22,87 @@ pub async fn state_diff_exex<Node: FullNodeComponents>(
         match notification {
             ExExNotification::ChainCommitted { new } => {
                 let tip_height = new.tip().number;
-                
+
                 println!("*\n*\n**This is a good place to find a city**\n*\n*");
+                //
+                //
+
+                let (current_block_num, _) = new.blocks().first_key_value().unwrap();
+
+                let previous_provider =
+                    ctx.provider().history_by_block_number(current_block_num - 1).unwrap();
+                let current_provider = ctx.provider().latest().unwrap();
+                let current_block =
+                    ctx.provider().block_by_number(current_block_num.clone()).unwrap().unwrap();
+                let current_block_txns = current_block
+                    .body
+                    .clone()
+                    .into_iter()
+                    .map(|tx| TransactionSignedNoHash::from(tx))
+                    .collect::<Vec<TransactionSignedNoHash>>();
+
+                let prev_block =
+                    ctx.provider().block_by_number(current_block_num - 1).unwrap().unwrap();
+                let prev_state_root = prev_block.state_root;
+
+                let previous_bundle_state = BundleState::default();
+                let current_bundle_state = &new.execution_outcome().bundle;
+
+                // let p1 = prev_prov.proof(&bs, addrs, &[]).unwrap();
+                // let p2 = prov.proof(&state, addrs, &[]).unwrap();
+                let mut parent_proofs: HashMap<Address, EIP1186AccountProofResponse> =
+                    HashMap::new();
+                let mut current_proofs: HashMap<Address, EIP1186AccountProofResponse> =
+                    HashMap::new();
+
+                for (address, _) in current_bundle_state.state() {
+                    let proof = previous_provider
+                        .proof(&previous_bundle_state, address.clone(), &[])
+                        .unwrap();
+                    let proof = from_primitive_account_proof(proof);
+                    parent_proofs.insert(address.clone(), proof);
+                }
+
+                for (address, _) in current_bundle_state.state() {
+                    let proof = current_provider
+                        .proof(&current_bundle_state, address.clone(), &[])
+                        .unwrap();
+                    let proof = from_primitive_account_proof(proof);
+                    current_proofs.insert(address.clone(), proof);
+                }
+
+                // TODO: continue from here:
+                let (state_trie, storage) = proofs_to_tries(
+                    prev_state_root.into(),
+                    parent_proofs.clone(),
+                    current_proofs.clone(),
+                )
+                .unwrap();
+
+                let input = SP1RethInput {
+                    beneficiary: current_block.header.beneficiary,
+                    gas_limit: current_block.gas_limit.try_into().unwrap(),
+                    timestamp: current_block.header.timestamp.try_into().unwrap(),
+                    extra_data: current_block.header.extra_data,
+                    mix_hash: current_block.header.mix_hash,
+                    transactions: current_block_txns,
+                    withdrawals: Vec::new(),
+                    parent_state_trie: state_trie,
+                    parent_storage: storage,
+                    contracts: Default::default(),
+                    parent_header: prev_block.header,
+                    ancestor_headers: Default::default(),
+                };
+
+                println!("input generation done now saving the file...");
+                let json_str = to_string(&input).unwrap();
+                // let mut file = File::create("output.json").unwrap();
+                // file.write_all(json_str.as_bytes()).unwrap();
+                let mut file = File::create(format!("{}.bin", current_block_num))
+                    .expect("Unable to open the file");
+                bincode::serialize_into(&mut file, &input).expect("Unable to serialize the input");
+                file.write_all(json_str.as_bytes()).unwrap();
+                println!("done saving the file :D");
 
                 // handle the state diffs
                 let new_diffs = extract_new_diffs(new);
